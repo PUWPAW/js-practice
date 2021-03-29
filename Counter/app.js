441b1d7e427c75f6fcbd053ecd0f42e7b838d303
Igor <ignatenkopo4ta@gmail.com>
2021-03-30 01:13:23 +0300

    counter app complited

--- Counter/app.js
+++ Counter/app.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+const incBtn = document.getElementById("incr");
+const decBtn = document.getElementById("decr");
+const resetBtn = document.getElementById("reset");
+const counter = document.querySelector(".counter");
+
+let currentCount = 0;
+
+incBtn.addEventListener("click", () => changeCounter(1));
+decBtn.addEventListener("click", () => changeCounter(-1));
+resetBtn.addEventListener("click", resetCounter);
+
+function changeCounter(num) {
+  counter.textContent = currentCount += num;
+
+  switch (true) {
+    case currentCount > 0:
+      counter.style.color = "white";
+      break;
+    case currentCount < 0:
+      counter.style.color = "darkred";
+      break;
+
+    default:
+      counter.style.color = "black";
+      break;
+  }
+}
+
+function resetCounter() {
+  currentCount = 0;
+  counter.textContent = currentCount;
+  counter.style.color = "black";
+}
